15fb7a10644aefee9da4f39d44160e38c01a072a
RobDGC <RDGC0002@ce.pucmm.edu.do>
2026-04-08 20:52:04 -0400

    -Implementacion de la ventana de inicio

--- src/main/java/org/gps/gpsproject/controladores/InicioControlador.java
+++ src/main/java/org/gps/gpsproject/controladores/InicioControlador.java
@@ -1,4 +1,19 @@
 package org.gps.gpsproject.controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import java.time.Year;
+
 public class InicioControlador {
+
+    @FXML private Label lblIntegrante1;
+    @FXML private Label lblIntegrante2;
+    @FXML private Label lblAnio;
+
+    @FXML
+    public void initialize() {
+        lblIntegrante1.setText("Robert Garcia");
+        lblIntegrante2.setText("Julian Espinal");
+        lblAnio.setText(String.valueOf(Year.now().getValue()));
+    }
 }
